c1ee2a499e2ed071eebd7715269a8e2f4e31c045
seanans <maks1998ivan@gmail.com>
2025-03-20 22:34:37 +0200

    test

--- test/car_test.py
+++ test/car_test.py
@@ -1,7 +1,9 @@
-import pytest
-import pygame
-import sys
 import os
+import sys
+
+import pygame
+import pytest
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..\project')))
 print("PYTHONPATH:", sys.path)
 from project.car import Ferrari458Italia
@@ -9,8 +11,8 @@ from project.obstacle import Obstacle
 from project.obstacle_manager import ObstacleManager
 from project.road import Road
 from project import constants
-from project.camera import Camera
 from unittest.mock import MagicMock
+
 
 # ================== Fixture for initialization ==================
 @pytest.fixture(scope='class', autouse=True)
@@ -21,7 +23,7 @@ def init_pygame_display(request):
     pygame.display.init()
     pygame.display.set_mode((1, 1))
     pygame.font.init()
-    
+
     yield
 
 
@@ -36,10 +38,12 @@ def obstacle_collision(self, depth, camera_offset_x):
     camera_offset_x = 0
     return obstacleManager._check_collision(self.car, road, camera_offset_x)
 
+
 class TestCar:
     """
     Test for the Car class
     """
+
     # ================== Sipmle tests ==================
 
     def setup_method(self):
@@ -65,46 +69,44 @@ class TestCar:
 
     @pytest.mark.parametrize("method, expected_sign", [
         ("move_right", 1),  # Expected steering angle (pos for right/neg for left)
-        ("move_left", -1)   
+        ("move_left", -1)
     ])
-
     def test_steering(self, method, expected_sign):
         getattr(self.car, method)()  # Call the method
-        assert expected_sign * self.car._Car__steering_angle > 0 
+        assert expected_sign * self.car._Car__steering_angle > 0
 
     @pytest.mark.parametrize("speed,expected_angle", [
-    (0, 20),      # Low speed, max angle
-    (100, 18),    # Mid speed
-    (200, 16),    # High speed, min angle
+        (0, 20),  # Low speed, max angle
+        (100, 18),  # Mid speed
+        (200, 16),  # High speed, min angle
     ])
-
     def test_max_steering_angle(self, speed, expected_angle):
         self.car.speed = speed
-        assert self.car._Car__get_max_steering_angle() == expected_angle  
+        assert self.car._Car__get_max_steering_angle() == expected_angle
 
-    # ================== Markered tests ==================
+        # ================== Markered tests ==================
 
     @pytest.mark.long
     def test_long_acceleration(self):
         for _ in range(500):
             self.car.increase_throttle()
             self.car._update_speed()
-    
-        assert self.car.speed > 200  
+
+        assert self.car.speed > 200
 
     @pytest.mark.long
     def test_slow_braking(self):
-        for _ in range(5):   # Accelerate
+        for _ in range(5):  # Accelerate
             self.car.increase_throttle()
             self.car._update_speed()
 
-        for i in range(1000): # Testing speed decrease without breaks, using physics
+        for i in range(1000):  # Testing speed decrease without breaks, using physics
             self.car.decrease_speed(constants.CAR_INERTIA_FACTOR)
 
         assert self.car.speed == pytest.approx(0, abs=1e-5)
 
     # ================== Mocking tests ==================
-    
+
     def test_apply_road_force(self):
         mock_road = MagicMock()
         mock_road.next_turn = "hard_left"
@@ -119,7 +121,7 @@ class TestCar:
     def test_update_position(self):
         mock_camera = MagicMock()
         mock_camera.get_position()
-    
+
         self.car._update_position(mock_camera)
-    
+
         mock_camera.get_position.assert_called_once()  # Ensure method was called
