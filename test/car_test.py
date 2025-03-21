401f41ef26dc8226926cafb426476fa85ddd8394
porokhniavitalii <legenda.repa4@gmail.com>
2025-03-21 12:02:31 +0200

    pytest #10
    path to project

--- test/car_test.py
+++ test/car_test.py
@@ -4,8 +4,8 @@ import sys
 import pygame
 import pytest
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../project')))
-print("PYTHONPATH:", sys.path)
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../project')))
+# print("PYTHONPATH:", sys.path)
 from project.car import Ferrari458Italia
 from project.obstacle import Obstacle
 from project.obstacle_manager import ObstacleManager
